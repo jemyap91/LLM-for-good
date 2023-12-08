7abfd123fda527f4138456d414b6855f712433b1
jemyap91 <j-emyap@hotmail.com>
2023-12-08 08:36:47 +0800

    Added reader for docx

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from langchain.document_loaders import Docx2txtLoader
 
 import streamlit as st
 
@@ -11,78 +12,103 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 import tempfile
+import docx2txt
 
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+# File upload
+allowed_file_types = ["docx", "txt", "pdf"]
+
 def generate_response(uploaded_file, openai_api_key, query_text):
+
+    # # File upload
+    # allowed_file_types = ["docx", "txt", "pdf"]
+
     # Load document if file is uploaded
     if uploaded_file is not None:
+
+        # Retrieve file extension
+        file_extension = uploaded_file.name.split(".")[-1].lower()
+
+        # Check if file extension allowed
+        if file_extension in allowed_file_types:
+
         # Read the content of the uploaded file as bytes
-        file_content = uploaded_file.read()
+            file_content = uploaded_file.read()
 
-        # Save the content to a temporary file
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-            temp_file.write(file_content)
-            temp_file_path = temp_file.name
+            # Save the content to a temporary file
+            with tempfile.NamedTemporaryFile(delete=False, suffix=f".{file_extension}") as temp_file:
+                temp_file.write(file_content)
+                temp_file_path = temp_file.name
 
-        loader = PyPDFLoader(temp_file_path)
-        #Load the document by calling loader.load()
-        docs = loader.load()
+            if file_extension == 'pdf':
+                loader = PyPDFLoader(temp_file_path)
+                #Load the document by calling loader.load()
+                docs = loader.load()
+            
+            elif file_extension == 'docx':
+                loader = Docx2txtLoader(temp_file_path)
+                docs = loader.load()
 
-        # Split documents into chunks
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size = 1500,
-            chunk_overlap = 150,
-            separators=["\n\n", "\n", " ", ""]
-        )        
-        splits = text_splitter.split_documents(docs)
-        
-        # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(
-            documents=splits,
-            embedding=embeddings)
+            # elif file_extension == 'txt':
+            #     docs = [uploaded_file.read().decode()]
 
-        # Build prompt
-        template = """Use the following pieces of context to answer the question at the end. \
-            If you don't know the answer, just say that you don't know, don't try to make up an answer. Use between 1-25 sentences unless there is a lot of information to synthesize. \
-                Keep the answer concise but do not leave out important details. Always end with "Thanks for asking me, the tl;dr bot 📖 🤖 !" at the end of the answer. 
-        {context}
-        Question: {question}
-        Helpful Answer:"""
-        QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+            # Split documents into chunks
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size = 1500,
+                chunk_overlap = 150,
+                separators=["\n\n", "\n", " ", ""]
+            )        
+            splits = text_splitter.split_documents(docs)
+            
+            # Select embeddings
+            embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+            
+            # Create a vectorstore from documents
+            db = Chroma.from_documents(
+                documents=splits,
+                embedding=embeddings)
 
-        # Create retriever interface
-        retriever = db.as_retriever()
+            # Build prompt
+            template = """Use the following pieces of context to answer the question at the end. \
+                If you don't know the answer, just say that you don't know, don't try to make up an answer. Use between 1-25 sentences unless there is a lot of information to synthesize. \
+                    Keep the answer concise but do not leave out important details. Always end with "Thanks for asking 📖 🤖 !" at the end of the answer. 
+            {context}
+            Question: {question}
+            Helpful Answer:"""
+            QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
-        # Create QA chain
-        llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
-        
-        qa_chain = RetrievalQA.from_chain_type(
-            llm,
-            chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-            retriever=retriever,
-            return_source_documents=False
-            )
-        
-        # Run chain
-        result = qa_chain({"query": query_text})
+            # Create retriever interface
+            retriever = db.as_retriever()
 
-        answer = result['result']
+            # Create QA chain
+            llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
+            
+            qa_chain = RetrievalQA.from_chain_type(
+                llm,
+                chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+                retriever=retriever,
+                return_source_documents=False
+                )
+            
+            # Run chain
+            result = qa_chain({"query": query_text})
 
-        return answer
+            answer = result['result']
+
+            return answer
 
 
 # Page title
 st.set_page_config(page_title='TL;DR Bot 📖 🤖')
 st.title('TL;DR Bot 📖 🤖')
 
-# File upload
-uploaded_file = st.file_uploader('Upload an article', type='pdf')
+
+uploaded_file = st.file_uploader('Hi, I am Jemmet, the tl;dr 🤖. To start, upload a document, ask a question and enter your OpenAI API Key',
+                                #  accept_multiple_files=True,
+                                  type=allowed_file_types)
 # Query text
 query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
